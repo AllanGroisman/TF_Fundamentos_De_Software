15a9438790113e436f4af391c60e4134ea71e49a
AllanGroisman <allan.groisman@edu.pucrs.br>
2023-11-13 21:59:54 -0300

    Esqueleto e imp inicial

--- src/main/java/br/fds/Aplicacao/Servico/EfetivarCompra_UC.java
+++ src/main/java/br/fds/Aplicacao/Servico/EfetivarCompra_UC.java
@@ -1,5 +1,24 @@
 package br.fds.Aplicacao.Servico;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.fds.Dominio.Entidades.ServicoEstoque;
+import br.fds.Dominio.Entidades.ServicoVenda;
+import br.fds.Dominio.Entidades.Entidades.Produto;
+
+@Component
 public class EfetivarCompra_UC {
-    
+    @Autowired
+    private ServicoEstoque servicoEstoque;
+    private ServicoVenda servicoVenda;
+
+    public boolean run() {
+        if(servicoVenda.efetivarCompra() && servicoEstoque.efetivarCompra()){
+            return true;
+        }        
+        return false;
+    }
 }
--- src/main/java/br/fds/Aplicacao/Servico/SolicOrcamento_UC.java
+++ src/main/java/br/fds/Aplicacao/Servico/SolicOrcamento_UC.java
@@ -1,5 +1,21 @@
 package br.fds.Aplicacao.Servico;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.fds.Dominio.Entidades.ServicoEstoque;
+import br.fds.Dominio.Entidades.ServicoVenda;
+import br.fds.Dominio.Entidades.Entidades.Orcamento;
+import br.fds.Dominio.Entidades.Entidades.Produto;
+
+@Component
 public class SolicOrcamento_UC {
-    
+    @Autowired
+    private ServicoVenda servicoVenda;
+
+    public Orcamento run() {
+        return servicoVenda.solicOrcamento();
+    }
 }
